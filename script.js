e89cc254b3a92834d1f9b501fd97d3d0d1b82625
Cosmin Emil Bucur <cosminemilbucur@Cosmins-MacBook-Air.local>
2025-11-13 11:20:24 +0200

    changed from switch to if/else

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function changeColor() {
+setInterval(function changeColor() {
     //alert("Ai apasat butonul");
     let button = document.getElementById('myBtn');
-    switch (button.className) {
-        case 'btn btn-danger mt-0':
-            button.className = 'btn btn-warning mt-0';
-            break;
-        case 'btn btn-warning mt-0':
-            button.className = 'btn btn-success mt-0';
-            break;
-        case 'btn btn-success mt-0':
-            button.className = 'btn btn-danger mt-0';
+
+    if (button.className == 'btn btn-danger mt-0') {
+        button.className = 'btn btn-warning mt-0';
+    } else if (button.className == 'btn btn-warning mt-0') {
+        button.className = 'btn btn-success mt-0';
+    } else if (button.className == 'btn btn-success mt-0') {
+        button.className = 'btn btn-danger mt-0';
     }
-}
+    //alert("a juns aici");
+}, 10*1000);
