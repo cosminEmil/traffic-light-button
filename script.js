73b02148f19c833464da6fca8a20df0f44926100
Cosmin Emil Bucur <cosminbucur2312@gmail.com>
2025-11-16 11:16:26 +0200

    Implement an array version of modifying the colors of the button

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function changeToRed() {
-    let button = document.getElementById('myBtn');
-    button.className = 'btn btn-danger mt-0';
-    setTimeout(changeToYellow, 10 * 1000, button);
-}
+let colors = ['btn btn-danger mt-0', 'btn btn-warning mt-0', 'btn btn-success mt-0']
+let cnt = 0;
 
-function changeToYellow(button) {
-    button.className = 'btn btn-warning mt-0';
-    setTimeout(changeToGreen, 10 * 1000, button);
-}
+setInterval(() => {
+    if (cnt == 3) {
+        cnt = 0;
+    }
+    changeColor();
+}, 10 * 1000);
 
-function changeToGreen(button) {
-    button.className = 'btn btn-success mt-0';
-    setTimeout(changeToRed, 10 * 1000);
+function changeColor() {
+    let btn = document.getElementById('myBtn');
+    btn.className = colors[cnt];
+    ++cnt;
 }
